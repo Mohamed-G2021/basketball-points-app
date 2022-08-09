7d1388150360438c849a41af41846675ed0ac04f
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2022-08-09 08:28:29 +0200

    created counterstates and countercubit

---
+++ lib/cubit/counter_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:basketball_points_app/cubit/counter_states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+class CounterCubit extends Cubit<CounterStates> {
+  CounterCubit(initialState) : super(initialState);
+}
---
+++ lib/cubit/counter_states.dart
@@ -0,0 +1,5 @@
+abstract class CounterStates {}
+
+class CounterAIncrementState extends CounterStates {}
+
+class CounterBIincrementState extends CounterStates {}
