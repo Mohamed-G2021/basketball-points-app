dc7a6dd921dcf5735b0665b46fcfb991c6058664
Mohamed-G2021 <mohamed_20191821@fci.helwan.edu.eg>
2022-08-09 10:41:05 +0200

    created cubit functions

--- lib/cubit/counter_cubit.dart
+++ lib/cubit/counter_cubit.dart
@@ -3,4 +3,17 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CounterCubit extends Cubit<CounterStates> {
   CounterCubit(initialState) : super(initialState);
+
+  int numOfPointsTeamA = 0;
+  int numOfPointsTeamB = 0;
+
+  void teamPointsIncrement({required String team, required int numOfPoints}) {
+    if (team == 'A') {
+      numOfPointsTeamA += numOfPoints;
+      emit(CounterAIncrementState());
+    } else {
+      numOfPointsTeamB += numOfPoints;
+      emit(CounterBIncrementState());
+    }
+  }
 }
